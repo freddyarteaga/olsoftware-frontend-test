5953beb86bd0fa3a521446860a7666e4a4547f5a
Freddy Arteaga <freddy.artea@gmail.com>
2021-03-11 00:39:45 -0500

    add: header

---
+++ src/components/header.js
@@ -0,0 +1,49 @@
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { FaUserCircle } from 'react-icons/fa'
+import { IoExitOutline } from 'react-icons/io5'
+
+export const Header = () => {
+  function hdlClickBurger() {
+    console.log('hdlClickBurger')
+  }
+
+  function hdlClickProfile() {
+    console.log('hdlClickProfile')
+  }
+
+  function hdlClickLogout() {
+    console.log('hdlClickLogout')
+  }
+
+  return (
+    <header className="Header">
+      <div className="Header-Side--left">
+        <GiHamburgerMenu 
+          color="#2644A7" 
+          onClick={hdlClickBurger} 
+          style={{ marginRight: 15 }} 
+        />
+        <h3 className="Header-Title">
+          Prueba Front-end
+        </h3>
+      </div>
+      <div className="Header-Side--right">
+        <FaUserCircle 
+          size={28} 
+          color="#2644A7" 
+          onClick={hdlClickProfile} 
+          style={{ marginRight: 25 }} 
+        />
+        <span>
+          Andrés Felipe Garcia Castro
+        </span>
+        <IoExitOutline 
+          size={24} 
+          color="#2644A7" 
+          onClick={hdlClickLogout} 
+          style={{ marginLeft: 25 }} 
+        />
+      </div>
+    </header>
+  )
+}
